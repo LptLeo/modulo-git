b84685e148524493eb32b08667b3aad83e26017d
Leonardo <lptleo11@gmail.com>
2025-11-05 19:15:34 -0400

    Desafio 02 concluído

--- desafios.py
+++ desafios.py
@@ -26,7 +26,8 @@ def listar_comandos_git_basicos():
     Exemplo de saída:
     ["git init", "git add", "git commit", "git status", "git push"]
     """
-    pass
+    comandos = ["git init", "git add", "git commit", "git status", "git push"]
+    print(f"Principais comandos do Git: {comandos}")
 
 
 def criar_mensagem_commit(funcao_nome):
